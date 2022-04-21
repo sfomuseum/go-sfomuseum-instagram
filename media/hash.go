cac33fc76f6830806663a84dff03c18b08d1f18e
thisisaaronland <aaron@localhost>
2022-04-21 11:10:32 -0700

    change signature of AppendHashes method to take AppendHashesOptions

--- media/hash.go
+++ media/hash.go
@@ -12,12 +12,22 @@ import (
 	"path/filepath"
 )
 
+// type AppendHashesOptions provides configuration options for appending hashes to an Instagram post.
+type AppendHashesOptions struct {
+	// A valid gocloud.dev/blob.Bucket where source media files associated with a post are found
+	Bucket *blob.Bucket
+	// A boolean flag indicating that a file hash (SHA-256) of the associated media file should be appended to an Instagram post
+	FileHash bool
+	// A boolean flag indicating that a perceptual of the associated media file should be appended to an Instagram post
+	PerceptualHash bool
+}
+
 // AppendHashes will append a variety of hashes to 'body' derived from its contents.
 // For images it will append a "file_hash" (SHA-256) and a "perceptual_hash" JSON properties.
 // For images it will append a "file_hash" (SHA-256) JSON property.
 // Media file URIs are derived from the "path" JSON property in 'body'. These URIs are expected
 // to be relative and resolvable in 'bucket'.
-func AppendHashes(ctx context.Context, body []byte, bucket *blob.Bucket) ([]byte, error) {
+func AppendHashes(ctx context.Context, opts *AppendHashesOptions, body []byte) ([]byte, error) {
 
 	updates := make(map[string]string)
 
@@ -29,7 +39,7 @@ func AppendHashes(ctx context.Context, body []byte, bucket *blob.Bucket) ([]byte
 
 	rel_path := path_rsp.String()
 
-	media_fh, err := bucket.NewReader(ctx, rel_path, nil)
+	media_fh, err := opts.Bucket.NewReader(ctx, rel_path, nil)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to open %s, %w", rel_path, err)
@@ -45,25 +55,31 @@ func AppendHashes(ctx context.Context, body []byte, bucket *blob.Bucket) ([]byte
 
 	media_r := bytes.NewReader(media_body)
 
-	file_hash, err := hash.FileHash(media_r)
+	if opts.FileHash {
 
-	if err != nil {
-		return nil, fmt.Errorf("Failed to generate file hash for %s, %w", rel_path, err)
-	}
-
-	updates["file_hash"] = file_hash
-
-	if filepath.Ext(rel_path) != ".mp4" {
-
-		media_r.Seek(0, 0)
-
-		p_hash, err := hash.PerceptualHash(media_r)
+		file_hash, err := hash.FileHash(media_r)
 
 		if err != nil {
-			return nil, fmt.Errorf("Failed to generate file perceptual for %s, %w", rel_path, err)
+			return nil, fmt.Errorf("Failed to generate file hash for %s, %w", rel_path, err)
 		}
 
-		updates["perceptual_hash"] = p_hash
+		updates["file_hash"] = file_hash
+	}
+
+	if opts.PerceptualHash {
+
+		if filepath.Ext(rel_path) != ".mp4" {
+
+			media_r.Seek(0, 0)
+
+			p_hash, err := hash.PerceptualHash(media_r)
+
+			if err != nil {
+				return nil, fmt.Errorf("Failed to generate file perceptual for %s, %w", rel_path, err)
+			}
+
+			updates["perceptual_hash"] = p_hash
+		}
 	}
 
 	for k, v := range updates {
